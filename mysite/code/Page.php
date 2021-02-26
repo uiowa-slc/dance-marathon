5e13513c4354064494d1e97ac54527d8071ba4c8
Dustin Quam <quamsta@gmail.com>
2021-02-26 10:56:00 -0600

    static publisher

--- mysite/code/Page.php
+++ mysite/code/Page.php
@@ -1,18 +1,20 @@
 <?php
 
-use SilverStripe\Forms\CheckboxField;
 use SilverStripe\AssetAdmin\Forms\UploadField;
 use SilverStripe\Assets\Image;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Control\Director;
+use SilverStripe\Forms\CheckboxField;
 use SilverStripe\Forms\HTMLEditor\HTMLEditorField;
 use SilverStripe\ORM\ArrayList;
+use SilverStripe\StaticPublishQueue\Contract\StaticallyPublishable;
 use SLC\Calendar\Calendar;
 
-class Page extends SiteTree {
+class Page extends SiteTree implements StaticallyPublishable {
 
 	private static $db = array(
-        'Sidebar' => 'HTMLText',
-        'ShowChildrenInDropdown' => 'Boolean(1)',
+		'Sidebar' => 'HTMLText',
+		'ShowChildrenInDropdown' => 'Boolean(1)',
 	);
 
 	private static $has_one = array(
@@ -29,17 +31,17 @@ class Page extends SiteTree {
 		//$fields->removeByName("Metadata");
 		$fields->addFieldToTab("Root.Main", new UploadField("Photo", "Photo"), "Content");
 
-        $fields->addFieldToTab("Root.Sidebar", new HTMLEditorField("Sidebar", "Sidebar Content"));
+		$fields->addFieldToTab("Root.Sidebar", new HTMLEditorField("Sidebar", "Sidebar Content"));
 
 		return $fields;
 
-    }
-    
-    public function getSettingsFields(){
-        $fields = parent::getSettingsFields();
-        $fields->addFieldToTab('Root.Settings', CheckboxField::create('ShowChildrenInDropdown', 'Show child pages in a dropdown menu if page is in the top bar (Yes)'));
-        return $fields;
-    }
+	}
+
+	public function getSettingsFields() {
+		$fields = parent::getSettingsFields();
+		$fields->addFieldToTab('Root.Settings', CheckboxField::create('ShowChildrenInDropdown', 'Show child pages in a dropdown menu if page is in the top bar (Yes)'));
+		return $fields;
+	}
 
 	public function Calendar() {
 		return Calendar::get()->First();
@@ -66,6 +68,31 @@ class Page extends SiteTree {
 		// print_r($catEvents);
 
 		return $catEventsSorted;
+
+	}
+	public function urlsToCache() {
+		$disallowedClasses = array(
+			'SilverStripe\CMS\Model\RedirectorPage',
+			'SilverStripe\UserForms\Model\UserDefinedForm',
+		);
+
+		//Only cache this year's previous lectures so the caching process doesn't go through the entire archive, while also invalidating recent events that featured the event being "live" the day before with the livestream links, etc
+		if ($this->ClassName == 'NewsEntry') {
+			$currentYear = date("Y");
+			$blogYear = $this->obj('Created')->Format('y');
+
+			if ($blogYear < $currentYear) {
+				return [];
+			} else {
+				return [Director::absoluteURL($this->getOwner()->Link()) => 0];
+			}
+		}
+
+		if (!array_search($this->ClassName, $disallowedClasses)) {
+			return [Director::absoluteURL($this->getOwner()->Link()) => 0];
+		} else {
+			return [];
+		}
 
 	}
 }
